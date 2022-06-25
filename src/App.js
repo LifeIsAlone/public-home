23e7f1832df8fc2a00a5bc9733e179be7fd77b3e
Park Award <parksangjun1363@gmail.com>
2022-06-25 15:42:06 +0900

    Spinner 생성 완료

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { NaverMap } from 'react-naver-maps'
 import Marker from './components/NaverMapTools/Marker';
 import './App.css';
 import { HomesProvider, initData } from './context/HomeContext';
+import Header from './components/Header';
+import Spinner from './components/Spinner';
 
 function App() {
   const nRef = useRef(null);
@@ -14,24 +16,30 @@ function App() {
 
   return (
     <div className="App">
+      <Spinner /> 
+
       <NaverMap
-        mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+// default: react-naver-map
         style={{
           width: '100%',
-          height: '100vw',
+          height: '100vh',
+          zIndex: -1,
         }}
         defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
         defaultZoom={12}
         naverRef={nRef}
       >
-        <Suspense fallback={<p>사용자 정보 로딩중...</p>}>
+        <Suspense fallback={<p>행복주택 정보 로딩중...</p>}>
           <HomesProvider resource={initData(nMap)}>
+            <Header/>
             <Marker />
           </HomesProvider>
         </Suspense>
       </NaverMap>
+
     </div>
   );
 }
+
 
 export default App;
