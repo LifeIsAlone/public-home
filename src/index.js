c5013fcb607435a55a31fe9bb1fe8cc043c29821
Park Award <parksangjun1363@gmail.com>
2022-06-25 20:08:39 +0900

    index.js RenderAferNavermap 태크 제거

--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { RenderAfterNavermapsLoaded } from "react-naver-maps";
 
-const API_KEY = process.env.REACT_APP_NAVER_MAP_API_KEY;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <RenderAfterNavermapsLoaded
-    clientId={API_KEY}
-    error={<p>Maps Load Error</p>}
-    loading={<p>Maps Loading</p>}
-  >
-    <App />
-  </RenderAfterNavermapsLoaded>
-);
 
+//RenderAfterNavermapsLoaded Secret, mobile Chrome 에서 오작동 => infinity Loading 됨
+root.render(
+    <App />
+);
+// 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
