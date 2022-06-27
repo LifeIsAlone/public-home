2f50728e3293508764d16d76c49697d3cd2a7a84
Park Award <parksangjun1363@gmail.com>
2022-06-27 20:53:59 +0900

    NaverMap 하위 컴포넌트 No Render 이슈 해결

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
 
   return (
     <div>
-      <Header />
       <NaverMap
         mapDivId={"maps-getting-started-uncontrolled"} // default: react-naver-map
         style={{
@@ -23,12 +22,14 @@ function App() {
         defaultCenter={{ lat: 37.5665734, lng: 126.978179 }}
         defaultZoom={12}
         mapState={(map) => setNMap(map)}
-      />
-      <Suspense fallback={<><Spinner text={"행복주택 데이터 수집중..."} /></>}>
-        <HomesProvider resource={initData(nMap)}>
-          <Marker />
-        </HomesProvider>
-      </Suspense>
+      >
+        <Suspense fallback={<><Spinner text={"행복주택 데이터 수집중..."} /></>}>
+          <HomesProvider resource={initData(nMap)}>
+            <Aside />
+            <Marker />
+          </HomesProvider>
+        </Suspense>
+      </NaverMap>
     </div>
   );
 }
