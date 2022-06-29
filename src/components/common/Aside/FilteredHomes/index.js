ce134b1f28a9eff2e00af8bb12b022c4cc0787a9
Park Award <parksangjun1363@gmail.com>
2022-06-29 16:32:45 +0900

    feat: insert designed scroll

--- src/components/common/Aside/FilteredHomes/index.js
+++ src/components/common/Aside/FilteredHomes/index.js
@@ -1,13 +1,34 @@
 
-import React, { useEffect } from 'react'
+import React, { useRef } from 'react'
 import { useHomesState } from './../../../../provider/HomeContext'
 import Items from './Items';
+import styled from 'styled-components'
 
 export default function FilteredHomes() {
+  const scroll = useRef(null);
   const state = useHomesState();
   return (
-    <ul>
+    <CustomUl ref={scroll}>
       {state.filteredHomes.map(item => <Items item={item} />)}
-    </ul>
+    </CustomUl>
   )
 }
+
+const CustomUl = styled.ul`
+  overflow: auto;
+  max-height: 88vh;
+  &::-webkit-scrollbar {
+    width: 8px; 
+}
+
+&::-webkit-scrollbar-thumb {
+    height: 30%; 
+    background: #217af4; 
+    
+    border-radius: 10px;
+}
+
+&::-webkit-scrollbar-track {
+    background: rgba(33, 122, 244, .1);  
+}
+`
