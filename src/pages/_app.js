f8a7991faa890a128619241e35037c8b4eed6b91
cyberjam <jamsince1994@naver.com>
2022-09-28 01:33:35 +0900

    🐛 ; 로 인해 스크롤 틀이 안맞는 이슈 해결

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
         </ThemeProvider>
     );
 }
