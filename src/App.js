2628c352866913fb2bcb263a6c8afa2feb52c18b
cyberjam <jamsince1994@naver.com>
2022-10-02 01:27:48 +0900

    ✨ 기존 사이트 폐쇄 및 url 안내 버튼

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import { useState, Suspense, useEffect } from "react";
-import Marker from "./components/NaverMapTools/Marker";
-import Spinner from "./components/common/Spinner";
-import Aside from "./components/common/Aside";
 
 import "./App.css";
-import { HomesProvider, initData } from "./provider/HomeContext";
-import { NaverMap } from "./components/NaverMapTools/NaverMap";
+import styled from "styled-components";
 
 function App() {
-  const [nMap, setNMap] = useState(null);
-
   return (
-    <div>
-      <NaverMap
-        mapDivId={"maps-getting-started-uncontrolled"} // default: react-naver-map
-        style={{
-          width: "100%",
-          height: "100vh",
-        }}
-        defaultCenter={{ lat: 37.5665734, lng: 126.978179 }}
-        defaultZoom={12}
-        mapState={(map) => setNMap(map)}
-      >
-        <Suspense fallback={<><Spinner text={"행복주택 데이터 수집중..."} /></>}>
-          <HomesProvider resource={initData(nMap)}>
-            <Aside />
-            <Marker />
-          </HomesProvider>
-        </Suspense>
-      </NaverMap>
-    </div>
+    <StyledMain>
+      <div>
+        <p>새로운 페이지로 새단장했어요!</p>
+        <a href="https://public-home.vercel.app/">
+          <button> 새로운 페이지 바로가기!</button>
+        </a>
+        
+      </div>
+    </StyledMain>
   );
 }
 
-
+const StyledMain = styled.div`
+  text-align:center;
+`
 export default App;
